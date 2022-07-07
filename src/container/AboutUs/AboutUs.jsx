33a84eb488e05df95081d198ed28f5262edb3aef
Nirmal Shah <nirmalshah2000@gmail.com>
2022-07-07 00:11:22 -0700

    Added AboutUs section with about & history content blocks with knife image in the middle and an image overlay using css positioning and z-index trickery.

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import constants from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus app__bg flex__center">
+    <div className="app__aboutus-overlay flex__center">
+      <img src={constants.images.G} alt="g letter" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img
+          src={constants.images.spoon}
+          alt="about_spoon"
+          className="spoon__img"
+        />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <button className="custom__button" type="button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={constants.images.knife} alt="knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img
+          src={constants.images.spoon}
+          alt="about_spoon"
+          className="spoon__img"
+        />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <button className="custom__button" type="button">
+          Know More
+        </button>
+      </div>
+    </div>
   </div>
 );
 
